d011d15a2eaa0dba5f732d33964edde0fb20abbf
Alex Benson <longroadhomeproductions@gmail.com>
2015-04-30 14:07:13 -0500

    initial commit

---
+++ dist/js/scripts.js
@@ -0,0 +1,1 @@
+"use strict";!function(window,document,$){$(document).ready(function(){$(document).foundation()})}(this,document,$);
---
+++ src/js/scripts.js
@@ -0,0 +1,11 @@
+'use strict';
+
+
+
+  (function(window, document, $) {
+
+    $(document).ready(function() {
+      $(document).foundation();
+    });
+
+  })(this, document, $);
